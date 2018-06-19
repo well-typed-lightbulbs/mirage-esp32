19419652a6e82ff644d4c6fc6f66c66ffe317072
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-06-19 15:35:32 +0100

    debug events

--- lib/event.ml
+++ lib/event.ml
@@ -1,3 +1,4 @@
+open Lwt.Infix
 
 external c_poll : [`Time] Time.Monotonic.t -> int -> int = "caml_poll"
 
@@ -21,15 +22,22 @@ let register_event_number num =
 
 let wait_for_event number = 
     assert (number >= 0 && number < 31);
+    Logs.info (fun f -> f "wait_for_event: %d %x" number !event_list);
     event_list := !event_list lor (1 lsl number);
-    Lwt_condition.wait (EventMap.find number !event_conditions)
+    Lwt_condition.wait (EventMap.find number !event_conditions) >>= fun _ ->
+    Logs.info (fun f -> f "wait_for_event triggered: %d" number);
+    Lwt.return_unit
 
 let work_is_available () = c_poll (Time.Monotonic.time ()) !event_list != 0
 
 let run () = 
     let rec check evt = function 
         | n when n < 31 -> 
-            if evt land (1 lsl n) != 0 then Lwt_condition.broadcast (EventMap.find n !event_conditions) ();
+            if evt land (1 lsl n) != 0 then 
+                begin
+                    Lwt_condition.broadcast (EventMap.find n !event_conditions) ();
+                    Logs.info (fun f -> f "poll triggered: %d" n);
+                end;
             check evt (n+1)
         | _ -> ()
     in

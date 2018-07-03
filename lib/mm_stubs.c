2e3d9542c4711c67753fba285c8d2f48d33fd47f
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-07-03 11:34:56 +0100

    remove debugs in memory usage functions.

--- lib/mm_stubs.c
+++ lib/mm_stubs.c
@@ -11,7 +11,6 @@ stub_heap_get_pages_total(__attribute__((unused)) value unit) // noalloc
 {
     multi_heap_info_t info;
     heap_caps_get_info(&info, MALLOC_CAP_DEFAULT);
-    fprintf(stderr, "mm_stubs.c: pages_total: watch out, not sure of this implementation!\n");
 	return Val_long(info.total_free_bytes + info.total_allocated_bytes);
 }
 
@@ -20,6 +19,5 @@ stub_heap_get_pages_used(__attribute__((unused)) value unit) // noalloc
 {
     multi_heap_info_t info;
     heap_caps_get_info(&info, MALLOC_CAP_DEFAULT);
-    fprintf(stderr, "mm_stubs.c: pages_used: watch out, not sure of this implementation!\n");
 	return Val_long(info.total_allocated_bytes);
 }
